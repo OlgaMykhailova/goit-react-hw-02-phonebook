2145d5bdd9ac46a9d2a3728dab7a297a250718b7
OlgaMykhailova <rosebud_z@yahoo.com>
2022-10-14 00:42:36 +0200

    added Step3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,19 @@ import { nanoid } from 'nanoid';
 import { Component } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-const initialValues = {
-  name: '',
-  number: '',
-};
-
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
     name: '',
     number: '',
   };
 
- 
   addContact = (values, actions) => {
     const newContact = {
       id: nanoid(),
@@ -29,16 +29,26 @@ export class App extends Component {
     actions.resetForm();
   };
 
-  handleSubmit = (values, actions) => {
-    console.log(values, actions);
-    actions.resetForm();
+  changeFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  getVisibleContacts = () => {
+    const contactList = this.state.contacts;
+    const normalizedFilter = this.state.filter.toLowerCase();
+    
+    return contactList.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
   };
 
   render() {
+    const visibleContacts = this.getVisibleContacts();
+
     return (
       <div>
         <h2>Phonebook</h2>
-        <Formik initialValues={initialValues} onSubmit={this.addContact}>
+        <Formik initialValues={this.state} onSubmit={this.addContact}>
           <Form>
             <label>
               <span>Name</span>
@@ -54,8 +64,18 @@ export class App extends Component {
           </Form>
         </Formik>
         <h2>Contacts</h2>
+
+        <label>
+          <span>Find contacts by name</span>
+          <input type="text" name="filter" onChange={this.changeFilter}></input>
+        </label>
+
         <ul>
-      {this.state.contacts.map(contact => (<li key={contact.id}>{contact.name}: {contact.number}</li>))}
+          {visibleContacts.map(contact => (
+            <li key={contact.id}>
+              {contact.name}: {contact.number}
+            </li>
+          ))}
         </ul>
       </div>
     );
