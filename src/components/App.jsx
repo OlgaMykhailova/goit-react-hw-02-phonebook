94a26f70fd2ce894f593c86c5dd7fc3774915340
OlgaMykhailova <rosebud_z@yahoo.com>
2022-10-13 23:28:57 +0200

    added Step1 and Step2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,63 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { nanoid } from 'nanoid';
+// import { propTypes } from 'prop-types';
+// import * as yup from 'yup';
+import { Component } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+const initialValues = {
+  name: '',
+  number: '',
 };
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+ 
+  addContact = (values, actions) => {
+    const newContact = {
+      id: nanoid(),
+      name: values.name,
+      number: values.number,
+    };
+
+    this.setState(({ contacts }) => ({ contacts: [newContact, ...contacts] }));
+
+    actions.resetForm();
+  };
+
+  handleSubmit = (values, actions) => {
+    console.log(values, actions);
+    actions.resetForm();
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <Formik initialValues={initialValues} onSubmit={this.addContact}>
+          <Form>
+            <label>
+              <span>Name</span>
+              <Field type="text" name="name"></Field>
+              <ErrorMessage name="name" component="span"></ErrorMessage>
+            </label>
+            <label>
+              <span>Number</span>
+              <Field type="tel" name="number"></Field>
+              <ErrorMessage name="number" component="span"></ErrorMessage>
+            </label>
+            <button type="submit">Add contact</button>
+          </Form>
+        </Formik>
+        <h2>Contacts</h2>
+        <ul>
+      {this.state.contacts.map(contact => (<li key={contact.id}>{contact.name}: {contact.number}</li>))}
+        </ul>
+      </div>
+    );
+  }
+}
